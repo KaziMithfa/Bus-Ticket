91749f510ebe19a701cc719d5b31548f288a17fa
KaziMithfa <kazimithfa99@gmail.com>
2024-09-13 12:01:54 +0200

    javaScript file added

---
+++ scripts/connection.js
@@ -0,0 +1,7 @@
+//using this function by clicking buy-tickets button we can scroll through the buying ticket section
+function Scroll() {
+  document.getElementById("target-section").scrollIntoView({
+    behavior: "smooth",
+    //we use this property to make the scroll smooth
+  });
+}
