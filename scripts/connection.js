2b98ac5e18297b7c3ad5d1cede2be320e10459cc
KaziMithfa <kazimithfa99@gmail.com>
2024-09-14 20:54:15 +0200

    color change of the selected seat completed

--- scripts/connection.js
+++ scripts/connection.js
@@ -5,3 +5,16 @@ function Scroll() {
     //we use this property to make the scroll smooth
   });
 }
+
+//it is the function of handlingSeat
+
+//Here we are doing code clicking on the seat to select it
+
+const allSeat = document.getElementsByClassName("select-option");
+let count = 0;
+for (const seat of allSeat) {
+  seat.addEventListener("click", function (event) {
+    const seatName = event.target.innerText;
+    changeColorofSeat(seatName);
+  });
+}
---
+++ scripts/utility.js
@@ -0,0 +1,5 @@
+function changeColorofSeat(id) {
+  const seatNumber = document.getElementById(id);
+  seatNumber.classList.remove("bg-[#F7F8F8]");
+  seatNumber.classList.add("bg-[#1DD100]");
+}
